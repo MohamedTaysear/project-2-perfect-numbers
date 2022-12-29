66c043873057f4ac3169620243fc7805e54f1d87
MohamedTaysear <121642186+MohamedTaysear@users.noreply.github.com>
2022-12-30 01:06:42 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the range of numbers to search for perfect numbers:");
+            Console.Write("Start: ");
+            int start = int.Parse(Console.ReadLine());
+            Console.Write("End: ");
+            int end = int.Parse(Console.ReadLine());
+
+            for (int i = start; i <= end; i++)
+            {
+                int sum = 0;
+                for (int j = 1; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        sum += j;
+                    }
+                }
+                if (sum == i)
+                {
+
+
+                    Console.WriteLine(i + " is a perfect number.");
+
+                }
+            }
+        }
+    }
+}
